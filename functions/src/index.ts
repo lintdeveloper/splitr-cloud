f476ef9474d7fe2ae890acbd35d90d188a5942cb
Musa Musa <musabrillz@gmail.com>
2020-08-09 09:40:19 +0100

    api/v1/login route added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -59,6 +59,43 @@ app.post('/register', async (req, res) => {
     }
 });
 
+// Login user
+app.post('/login', async (req, res, next) => {
+    try { 
+        const {firstName, lastName, email, photoUrl} = req.body;
+        
+        const userRef = db.collection(userCollection).doc(email);
+        const user = await userRef.get()
+
+        if (user.exists) {
+            // update the user data
+            const newUser: User = {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                photoUrl: photoUrl
+            }
+
+            await userRef.update(newUser);
+
+            res.status(200).send({
+                status: true,
+                message: "User logged In successfully !!!"
+            }); 
+        } else {
+            res.status(400).send({
+                status: false,
+                message: "User unavailable check your mail and try again"
+            }); 
+        }
+        
+    } catch (error) {
+        res.status(400).send({
+            status: false,
+            message: "Unable to login check your credentials and try again"
+        })
+    }
+})
 
 //define google cloud function name
 export const test = functions.https.onRequest(main);
